51b7bb83cb8d88e513a2540bf715bf84db8d63aa
PraveenMSD <praveenrk189@gmail.com>
2021-02-23 20:47:50 +0530

    Initial committ

--- src/config/Config.js
+++ src/config/Config.js
@@ -4,13 +4,7 @@ import "firebase/firestore";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 var firebaseConfig = {
-  apiKey: "AIzaSyCeOry-LbVV-fDOf0ysiSZuKpbaZwXLz2k",
-  authDomain: "djamware-17035.firebaseapp.com",
-  projectId: "djamware-17035",
-  storageBucket: "djamware-17035.appspot.com",
-  messagingSenderId: "440857038975",
-  appId: "1:440857038975:web:8f675252d3baa415f96f4a",
-  measurementId: "G-3DSKJTQTM6"
+// Firestore config
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
